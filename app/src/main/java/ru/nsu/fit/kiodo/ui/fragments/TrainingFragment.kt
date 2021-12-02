93406cd0bbe8430cfe34452430fd10aa817e10de
r.nikolaev <r.nikolaev@g.nsu.ru>
2021-12-02 21:05:50 +0700

    wip

--- app/src/main/java/ru/nsu/fit/kiodo/ui/fragments/TrainingFragment.kt
+++ app/src/main/java/ru/nsu/fit/kiodo/ui/fragments/TrainingFragment.kt
@@ -1,19 +1,45 @@
 package ru.nsu.fit.kiodo.ui.fragments
 
 import android.os.Bundle
+import android.view.*
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import ru.nsu.fit.kiodo.R
 
 class TrainingFragment : Fragment() {
+
+    private var progressBar : ProgressBar? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true);
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_training, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_training, container, false)
+        progressBar = view.findViewById(R.id.training_p_par)
+        return view
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.upper_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.back -> {
+//                todo return back action
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
